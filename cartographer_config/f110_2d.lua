f47b6e614bbd6d2f000bfb44e6f11972bcc540ad
mokelly <travelbureu@github.com>
2018-10-30 17:27:17 -0400

    update config bugs

--- cartographer_config/f110_2d.lua
+++ cartographer_config/f110_2d.lua
@@ -5,7 +5,7 @@ options = {
   map_builder = MAP_BUILDER,
   trajectory_builder = TRAJECTORY_BUILDER,
   map_frame = "map",
-  tracking_frame = "base_imu_link",
+  tracking_frame = "base_link",
   published_frame = "base_link",
   odom_frame = "odom",
   provide_odom_frame = false,
@@ -13,7 +13,7 @@ options = {
   use_nav_sat = false,
   num_laser_scans = 1,
   num_multi_echo_laser_scans = 0,
-  num_subdivisions_per_laser_scan = 2,
+  num_subdivisions_per_laser_scan = 1,
   num_point_clouds = 0,
   lookup_transform_timeout_sec = 0.2,
   submap_publish_period_sec = 0.3,
@@ -23,15 +23,21 @@ options = {
   odometry_sampling_ratio = 1.,
   fixed_frame_pose_sampling_ratio = 1.,
   imu_sampling_ratio = 1.,
+  use_landmarks = false,
+  publish_frame_projected_to_2d = false,
+  landmarks_sampling_ratio = 1.,
 }
 
 MAP_BUILDER.use_trajectory_builder_2d = true
-MAP_BUILDER.num_background_threads = 4.0
-TRAJECTORY_BUILDER_2D.use_imu_data = true
+MAP_BUILDER.use_trajectory_builder_3d = false
+MAP_BUILDER.num_background_threads = 5.0
+TRAJECTORY_BUILDER_2D.use_imu_data = false
+TRAJECTORY_BUILDER_2D.max_range = 25.0
+TRAJECTORY_BUILDER_2D.min_range = 0.5
 
 -- might be able to optimize these parameters
 -- see: http://google-cartographer-ros.readthedocs.io/en/latest/tuning.html
--- TRAJECTORY_BUILDER_2D.submaps.num_range_data = 100
--- POSE_GRAPH.optimize_every_n_nodes = 10
+TRAJECTORY_BUILDER_2D.submaps.num_range_data = 100
+POSE_GRAPH.optimize_every_n_nodes = 20
 
 return options
